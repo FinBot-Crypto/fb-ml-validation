fafded31ac8953fde50c08c243407e44974e5ed2
Renan Barbosa <renan_vianna7@icloud.com>
2026-04-30 19:09:29 -0300

    feat: implementacao inicial com pipeline compartilhado

---
+++ src/main.py
@@ -0,0 +1,14 @@
+﻿import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("ml-validation-service")
+
+def main():
+    logger.info("Service ml-validation-service starting...")
+    while True:
+        logger.info("Service ml-validation-service is running...")
+        time.sleep(60)
+
+if __name__ == "__main__":
+    main()
